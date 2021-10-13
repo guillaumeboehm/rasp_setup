71b8c5f006f170a722a0a874169377f6843d7b8a
Guillaume BOEHM <guillaumeboehm@hotmail.fr>
2021-10-13 15:48:22 +0200

    zsh fix

--- init.sh
+++ init.sh
@@ -33,9 +33,10 @@ super chsh -s /bin/zsh ubuntu || exit
 #? MongoDB install
 
 # Install the MongoDB 4.4 GPG key:
-wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | super apt-key add -
+super true # just to use sudo on the next lines
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 # Add the source location for the MongoDB packages:
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | super tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 
 # Download the package details for the MongoDB packages:
 super apt-get update || exit
