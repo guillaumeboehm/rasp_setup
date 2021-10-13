12d0cccccf667488cf329e11db01eb26117620eb
Guillaume BOEHM <guillaumeboehm@hotmail.fr>
2021-10-13 15:26:09 +0200

    zsh fix

--- init.sh
+++ init.sh
@@ -19,14 +19,15 @@ do
     fi
 done
 
-super dpkg --configure -a
-super apt update && super apt upgrade -y
+super dpkg --configure -a || exit
+super apt update && super apt upgrade -y || exit
 
 #? quality of life stuff
 
-super apt install -y zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-cp -r .zshrc ~
+super apt install -y zsh || exit
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || exit
+cp -r .zshrc ~ || exit
+chsh -s /bin/zsh ubuntu || exit
 
 #? MongoDB install
 
@@ -36,16 +37,16 @@ wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | super apt-key add
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | super tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 
 # Download the package details for the MongoDB packages:
-super apt-get update
+super apt-get update || exit
 # Install MongoDB:
-super apt-get install -y mongodb-org
+super apt-get install -y mongodb-org || exit
 # Ensure mongod config is picked up:
-super systemctl daemon-reload
+super systemctl daemon-reload || exit
 # Tell systemd to run mongod on reboot:
-super systemctl enable mongod
+super systemctl enable mongod || exit
 # Start up mongod!
-super systemctl start mongod
+super systemctl start mongod || exit
 
 #? node stuff
 
-super apt install nodejs npm
+super apt install -y nodejs npm || exit
